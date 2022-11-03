4ac3cd4a59465794e87accc3e8138f8ed3257d8f
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-04 01:58:22 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const containerGallery = document.querySelector('.gallery');
+
+function addGallery(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<a class = "gallery__item" href = "${original}">
+    <img class = "gallery__image" src="${preview}" alt="${description}"/>
+    </a>`
+    )
+    .join('');
+}
+containerGallery.insertAdjacentHTML('beforeend', addGallery(galleryItems));
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
