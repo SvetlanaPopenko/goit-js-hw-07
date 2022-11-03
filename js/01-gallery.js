3626e2e3728d63ac5916de7c91576400214177f5
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-04 00:26:28 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,40 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const containerGallery = document.querySelector('.gallery');
 
-containerGallery.addEventListener('click', onClickGallery);
-
-function onClick(arr) {
-  return arr.map(({ preview, original, description }) =>
-    `<div class = "gallery__item">
+function addGallery(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => 
+        `<div class = "gallery__item">
     <a class = "gallery__link" href = "${original}">
-    <img class = "gallery__image" src="${preview}" data - source = "${original}" alt="${description}"/>
+    <img class = "gallery__image" src="${preview}" data-source = "${original}" alt="${description}"/>
     </a>
-    </div>`).join('');
- }
-containerGallery.insertAdjacentHTML('beforeend', onClick(galleryItems));
+    </div>`
+      )
+    .join('');
+}
+containerGallery.insertAdjacentHTML('beforeend', addGallery(galleryItems));
 
 console.log(basicLightbox);
-function onClickGallery(evt) {
-  evt.preventDefault();
-  if (evt.target.nodeName !== 'IMG') {
-    return
+
+containerGallery.addEventListener('click', onClickGallery);
+
+function onClickGallery(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
   }
-  const originalEl = basicLightbox.create(`
+  const originalEl = basicLightbox.create(
+    `
   <div class="modal">
-		<img width="1400" height="900" src="${evt.target.dataset.source}">
+		<img width="1200" src="${e.target.dataset.source}">
 	</div>`,
-    { onShow: (instance) => {
-      containerGallery.addEventListener('keydown', onAddModalKeydown);
-     },
-    onClose: (instance) => {
-      containerGallery.removeEventListener('keydown', onAddModalKeydown);
-     },}
+    {
+      onShow: originalEl => {
+        containerGallery.addEventListener('keydown', onAddModalKeydown);
+      },
+      onClose: originalEl => {
+        containerGallery.removeEventListener('keydown', onAddModalKeydown);
+      },
+    }
   );
   originalEl.show();
-  
-    function onAddModalKeydown(evt) {
-      if (evt.code === "Escape") {
-        originalEl.close();
-      }
+
+  function onAddModalKeydown(evt) {
+    if (evt.code === 'Escape') {
+      originalEl.close();
     }
+  }
 }
