650da1920fb2bbfa06150414aaff8dc599df6d34
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-03 16:02:37 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const containerGallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+containerGallery.addEventListener('click', onClickGallery);
+
+function onClick(arr) {
+  return arr.map(({ preview, original, description }) =>
+    `<div class = "gallery__item">
+    <a class = "gallery__link" href = "${original}">
+    <img class = "gallery__image" src="${preview}" alt="${description}" data - source = "${original}"/>
+    </a>
+    </div>`).join('');
+ }
+containerGallery.insertAdjacentHTML('beforeend', onClick(galleryItems));
+
+console.log(basicLightbox);
+function onclickGallery(evt) {
+  
+}
